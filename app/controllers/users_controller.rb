a73bff416a7f2f69572165f011d130f30f8a104a
javonesm89 <javosmith12@gmail.com>
2021-01-25 14:08:32 -0500

    Updates to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
     def index
     end
 
+    def show
+        @user = User.find_by(params[:id])
+    end
+
     def new
         @user = User.new
     end
@@ -9,6 +13,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             redirect_to root_path
