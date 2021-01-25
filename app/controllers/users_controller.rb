d0f94d0ba76f3e6a8e4d6dc8776a07b75bc68f6c
javonesm89 <javosmith12@gmail.com>
2021-01-25 14:05:23 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,12 @@ class UsersController < ApplicationController
     end
 
     def create
-        binding.pry
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_path(@user)
+        else
+            redirect_to root_path
+        end
     end
 
     private
