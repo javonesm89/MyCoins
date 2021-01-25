fb5066decd383713787d17d67065f3b91d85b821
javonesm89 <javosmith12@gmail.com>
2021-01-25 14:50:56 -0500

    Updates to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
     def create
-        if User.exists(:email => params[:email]) && @user = User.find_by(:email => params[:email])
-            @user
+        if User.exists?(:email => params[:email]) && @user = User.find_by(:email => params[:email])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
-            flash[alert] = "Unable to find user"
-
+            flash[:alert] = "Unable to find user"
+            redirect_to login_path
+        end
     end
 
     def destroy
