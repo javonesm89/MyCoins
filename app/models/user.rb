3c7eba05fe6ef30e6d1a5401e8bed219c758571d
javonesm89 <javosmith12@gmail.com>
2021-01-26 15:03:43 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,33 +10,46 @@ class User < ApplicationRecord
 
 
     def adjust_for_taxes
-        binding.pry
-        salary = self.salary
-        case salary
-        when 10001..40000
+        case self.salary
+        when (10001..40000)
             taxable_amount = salary - 10000
-            money_to_sam = taxable_amount * .12 + 1,000
+            money_to_sam = taxable_amount * 0.12 + 1000
+            binding.pry
             new_salary = salary - money_to_sam
-        when 40001..90000
+        when (40001..90000)
             taxable_amount = salary - 40000
-            money_to_sam = taxable_amount * .22 + 4,700
+            money_to_sam = taxable_amount * 0.22 + 4700
             new_salary = salary - money_to_sam
-        when 90001..180000
+        when (90001..180000)
             taxable_amount = salary - 90000
-            money_to_sam = taxable_amount * .24 + 14,700
+            money_to_sam = (taxable_amount * 0.24) + (14700)
+            # binding.pry
             new_salary = salary - money_to_sam
-        when 180,001..300,000
+        when (180001..300000)
             taxable_amount = salary - 180000
-            money_to_sam = taxable_amount * .22 + 1,000
+            money_to_sam = (taxable_amount * 0.22) + (1000)
             new_salary = salary - money_to_sam
-        when 300,001..500000
+        when (300001..500000)
             taxable_amount = salary - 10000
-            money_to_sam = taxable_amount * .22 + 1,000
+            money_to_sam = taxable_amount * 0.22 + 1000
             new_salary = salary - money_to_sam
-        else 0..10
-            taxable_amount = salary - 10000
-            money_to_sam = taxable_amount * .22 + 1,000
-            new_salary = salary - money_to_sam
+        else (0..10)
+            nil
+            # taxable_amount = salary - 10000
+            # money_to_sam = taxable_amount * .22 + 1,000
+            # new_salary = salary - money_to_sam
         end
+    end
+
+    def medicare_tax(salary)
+        if salary >= 200000
+            medicare_tax = 0.145
+        else
+            medicare_tax = 0.29 
+        end 
+    end
+
+    def social_security_tax
+        social_security_tax = 6.2
     end
 end
